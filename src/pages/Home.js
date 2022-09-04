920211c8afc8ce2328299109811cae59fdb234fb
mudit-rajput <muditrajput13@gmail.com>
2022-09-04 23:40:46 +0530

    misc config file with api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,16 @@
 import React, { useState} from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
 
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const onSearch = () => {
-    fetch('https://api.tvmaze.com/search/shows?q=${input}')
-    .then(r => r.json())
-    .then(result => {
-      console.log(result);
+
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
     });
   };
 
@@ -21,13 +22,32 @@ const Home = () => {
     if(ev.keycode === 13){
       onSearch();
     }
-  }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Results</div>;
+    }
+
+    if(results && results.length > 0) {
+      return (
+      <div>
+        {results.map(item => (
+          <div key={item.show.id}>{item.show.name}</div>
+        ))}
+      </div>
+      );
+    }
+
+    return null;
+  };
 
   
   return (
     <MainPageLayout>
       <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
       <button type="button" onClick={onSearch}>Search</button>
+      {renderResults()};
     </MainPageLayout>
   )
 }
