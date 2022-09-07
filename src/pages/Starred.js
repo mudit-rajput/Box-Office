c699ecd7bda19e59ec4d7be68951e428504cb2fd
mudit-rajput <muditrajput13@gmail.com>
2022-09-08 00:04:38 +0530

    displaying starred shows - 1

--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
-
+import {useShows} from '../misc/custom-hooks'
 const Starred = () => {
-  return (
-    <MainPageLayout>Starred</MainPageLayout>
-  )
+
+  const[starred] = useShows();
+
+  const [shows, setShows] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (starred && starred.length > 0){
+
+    } else {
+      setIsLoading(false);
+    }
+  }, [starred])
+  
+
+
+  return  <MainPageLayout>Starred</MainPageLayout>
+  
 }
 
 export default Starred
