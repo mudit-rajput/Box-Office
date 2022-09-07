821ed1d9893959a8fa136a5871fff8f5e272dd4e
mudit-rajput <muditrajput13@gmail.com>
2022-09-08 01:56:38 +0530

    finishing stying 1

--- src/components/Navs.js
+++ src/components/Navs.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { NavList,LinkStyled } from './Navs.styled';
 
 
 const LINKS = [
@@ -7,14 +8,16 @@ const LINKS = [
     {to: '/starred' , text: 'Starred'},
 ]
 const Navs = () => {
+
+  const location = useLocation()
   return (
     <div>
-        <ul>
+        <NavList>
             {
                 LINKS.map(item => <li key={item.to}>
-                    <Link to={item.to}>{item.text}</Link></li> )
+                    <LinkStyled to={item.to} className={item.to === location.pathname ? 'active' : ''}>{item.text}</LinkStyled></li> )
             }
-        </ul>
+        </NavList>
     </div>
   )
 }
